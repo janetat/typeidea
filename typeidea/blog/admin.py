31c321f5280db0199a087ec43a32cb7be039988f
allen03 <aa704538319@163.com>
2019-04-14 22:20:18 +0800

    admin-后台数据只显示当前用户的-(但是新增页面还没完成只显示当前用户的数据)

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -8,7 +8,7 @@ from .models import Post, Category, Tag
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
+    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count', 'owner')
     fields = ('name', 'status', 'is_nav')
 
     def post_count(self, obj):
@@ -20,22 +20,31 @@ class CategoryAdmin(admin.ModelAdmin):
         obj.owner = request.user
         return super().save_model(request, obj, form, change)
 
+    def get_queryset(self, request):
+        query_set = super().get_queryset(request)
+        return query_set.filter(owner=request.user)
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status', 'created_time')
+    list_display = ('name', 'status', 'created_time', 'owner')
     fields = ('name', 'status')
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super().save_model(request, obj, form, change)
 
+    def get_queryset(self, request):
+        query_set = super().get_queryset(request)
+        return query_set.filter(owner=request.user)
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    # TODO 新增 文章时候 分类、标签只显示当前用户的
     list_display = [
         'title', 'category', 'status',
-        'created_time', 'owner', 'operator'
+        'created_time', 'owner', 'operator',
     ]
     list_display_links = []
     list_filter = [CategoryOwnerFilter]
@@ -46,9 +55,9 @@ class PostAdmin(admin.ModelAdmin):
     # actions_on_bottom = True
 
     # save_on_top = True
-
     fields = (
-        ('category', 'title'),
+        'category',
+        'title',
         'desc',
         'status',
         'content',
@@ -66,3 +75,7 @@ class PostAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super().save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        query_set = super().get_queryset(request)
+        return query_set.filter(owner=request.user)
