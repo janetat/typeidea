c7be4a875019dbd48f3be4d6bfad5a988a94363d
allen03 <aa704538319@163.com>
2019-04-16 20:21:34 +0800

    book/08 - 套bootstrap模板和配置static files path

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# 多主题切换， 在TEMPLATES配置
+THEME = 'bootstrap'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'typeidea.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['typeidea/templates', ],
+        'DIRS': [os.path.join(BASE_DIR, 'themes', THEME, 'templates') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'themes', THEME, "static"),
+]
