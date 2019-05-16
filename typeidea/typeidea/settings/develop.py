b1607c8ddddebe67a11e0b5df5afbf21f56092f1
allen03 <aa704538319@163.com>
2019-05-16 22:18:04 +0800

    book11 - django debug toolbar

--- typeidea/typeidea/settings/develop.py
+++ typeidea/typeidea/settings/develop.py
@@ -8,3 +8,17 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_CONFIG = {
+    'JQUERY_URL': 'https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js',
+}
--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -39,24 +39,32 @@ router.register(r'post', PostViewSet, basename='api-post')
 router.register(r'category', CategoryViewSet, basename='api-category')
 
 urlpatterns = [
-    path('super_admin/', admin.site.urls, name='super-admin'),
-    path('admin/', custom_site.urls, name='admin'),
-    path('xadmin/', xadmin.site.urls, name='xadmin'),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('api/', include(router.urls)),
-    path('api/docs/', include_docs_urls(title='typeidea apis')),
+                  path('super_admin/', admin.site.urls, name='super-admin'),
+                  path('admin/', custom_site.urls, name='admin'),
+                  path('xadmin/', xadmin.site.urls, name='xadmin'),
+                  path('ckeditor/', include('ckeditor_uploader.urls')),
+                  path('api/', include(router.urls)),
+                  path('api/docs/', include_docs_urls(title='typeidea apis')),
 
-    re_path(r'^$', IndexView.as_view(), name='index'),
-    re_path(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
-    re_path(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
-    re_path(r'^post/(?P<post_id>\d+)/$', PostDetailView.as_view(), name='post-detail'),
-    re_path(r'^links/$', LinkListView.as_view(), name='links'),
-    re_path(r'^comment/$', CommentView.as_view(), name='comment'),
-    re_path(r'^search/$', SearchView.as_view(), name='search'),
-    re_path(r'^author/(?P<owner_id>\d+)/$', AuthorView.as_view(), name='author'),
-    re_path(r'^rss|feed$', LatestPostFeed(), name='rss'),
-    re_path(r'^sitemap\.xml$', sitemap_views.sitemap, {'sitemaps': {'posts': PostSiteMap}}),
-    re_path(r'^category-autocomplete/$', CategoryAutocompleteView.as_view(), name='category-autocomplete'),
-    re_path(r'^tag-autocomplete/$', TagAutocompleteView.as_view(), name='tag-autocomplete'),
+                  re_path(r'^$', IndexView.as_view(), name='index'),
+                  re_path(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
+                  re_path(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
+                  re_path(r'^post/(?P<post_id>\d+)/$', PostDetailView.as_view(), name='post-detail'),
+                  re_path(r'^links/$', LinkListView.as_view(), name='links'),
+                  re_path(r'^comment/$', CommentView.as_view(), name='comment'),
+                  re_path(r'^search/$', SearchView.as_view(), name='search'),
+                  re_path(r'^author/(?P<owner_id>\d+)/$', AuthorView.as_view(), name='author'),
+                  re_path(r'^rss|feed$', LatestPostFeed(), name='rss'),
+                  re_path(r'^sitemap\.xml$', sitemap_views.sitemap, {'sitemaps': {'posts': PostSiteMap}}),
+                  re_path(r'^category-autocomplete/$', CategoryAutocompleteView.as_view(),
+                          name='category-autocomplete'),
+                  re_path(r'^tag-autocomplete/$', TagAutocompleteView.as_view(), name='tag-autocomplete'),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+                      path(r'__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
